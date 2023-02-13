0a2714f7960d82eeee4beb2da1bfab9648ec9562
Andrew Lang <112670330+asperatusx@users.noreply.github.com>
2023-02-13 03:47:29 -0500

    setup basic routes

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const customizeRoutes = require('./routes/customize')
 
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/customize', customizeRoutes);
 
 
 app.listen(8080, function() {
