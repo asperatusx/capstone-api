48ae2a7d173ecdd3354676382827cf5ad268a2cc
Andrew Lang <112670330+asperatusx@users.noreply.github.com>
2023-02-13 04:33:01 -0500

    add post request to add skills

--- routes/customize.js
+++ routes/customize.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+
+function getSkills() {
+  const skillsFromFile = fs.readFileSync('./data/skills.json');
+  return JSON.parse(skillsFromFile);
+}
+
+router.post('/', (req, res) => {
+  const skillsList = getSkills();
+  console.log(req.body.skills);
+  skillsList.skills.push(req.body.skills);
+  fs.writeFileSync('./data/skills.json', JSON.stringify(skillsList));
+  res.json(skillsList);
+})
+
+
+module.exports = router;
