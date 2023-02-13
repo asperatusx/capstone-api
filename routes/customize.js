af0b882261a66a7d3b38ef6254cca4ebc032ef77
Andrew Lang <112670330+asperatusx@users.noreply.github.com>
2023-02-13 15:55:00 -0500

    add dynamic projects and post request

--- routes/customize.js
+++ routes/customize.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 function getResume() {
   const resumeFromFile = fs.readFileSync('./data/resume.json');
@@ -9,8 +10,19 @@ function getResume() {
 
 router.post('/', (req, res) => {
   const resume = getResume();
-  console.log(req.body.skills);
-  resume.skills.push(req.body.skills);
+  if (req.body.skills) {
+    resume.skills.push(req.body.skills);
+  }
+  if ((req.body.title||req.body.image||req.body.demo)) {
+    const newProject = {
+      id: uuidv4(),
+      title: req.body.title || "Add a project title",
+      image: req.body.image || "http://localhost:8080/images/project1.jpg",
+      demo: req.body.demo || "https://www.heroku.com/"
+    }
+    resume.projects.push(newProject);
+  }
+
   fs.writeFileSync('./data/resume.json', JSON.stringify(resume));
   res.json(resume);
 })
