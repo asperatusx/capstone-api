9239667fe2ef5671eef846b2bc4a3b27a4f32638
Andrew Lang <112670330+asperatusx@users.noreply.github.com>
2023-02-15 02:25:23 -0500

    add delete for projects

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+function getResume() {
+  const resumeFromFile = fs.readFileSync('./data/resume.json');
+  return JSON.parse(resumeFromFile);
+}
+
 function getResumeById(id) {
   const resumeFromFile = fs.readFileSync('./data/resume.json');
   const resumes = JSON.parse(resumeFromFile);
@@ -9,7 +14,6 @@ function getResumeById(id) {
 }
 
 router.get('/:id', (req, res) => {
-
   const isCurrent =  req?.user?.id === req.params.id
 
   const resume = getResumeById(req.params.id);
@@ -17,7 +21,23 @@ router.get('/:id', (req, res) => {
   res.json({resume, isCurrent} ) ;
 })
 
+router.delete('/:id', (req, res) => {
+  const resumeList = getResume();
+  const resume = resumeList.find(resume => resume.id === req.user.id);
 
+  if (req.body.skill) {
+    const index = resume.skills.indexOf(req.body.skill);
+    resume.skills.splice(index, 1);
+  }
+  if (req.body.key) {
+    const filter = (element) => element.id ===req.body.key;
+    const index = resume.projects.findIndex(filter);
+    resume.projects.splice(index, 1);
+  }
+  
+  fs.writeFileSync('./data/resume.json', JSON.stringify(resumeList));
+  res.sendStatus(201);
+})
 
 
 
